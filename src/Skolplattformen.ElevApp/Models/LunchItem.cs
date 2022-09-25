63b4a672db4c1e777b5ee2ad8aa927d041ba1be5
Kajetan Kazimierczak <kajetan@hotmail.com>
2022-09-25 21:38:57 +0200

    LunchPage

---
+++ src/Skolplattformen.ElevApp/Models/LunchItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Skolplattformen.ElevApp.Models
+{
+    public class LunchItem
+    {
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public bool IsToday => Date.Date == DateTime.Now.Date;
+    }
+}
